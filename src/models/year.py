c87bc4933a26948e2ae7dc61bc1a7891b19360a2
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-04-14 14:51:22 -0700

    much higher learning rate

--- src/models/year.py
+++ src/models/year.py
@@ -44,7 +44,7 @@ class year_ensemble(LightningModule):
         return x
     
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.config["lr"])
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.1)
         
         return optimizer
         

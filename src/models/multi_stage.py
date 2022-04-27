412654ee32a30df4f2157566e9ba0c857a7f0358
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-04-27 14:50:45 -0700

    error in processing

--- src/models/multi_stage.py
+++ src/models/multi_stage.py
@@ -241,8 +241,8 @@ class MultiStage(LightningModule):
         level_results = []
         for index, level in enumerate(predict_df):
             # Concat batches            
-            individuals = np.concatenate([y[0] for y in level for x in level])
-            predictions = np.vstack([y[1].cpu() for y in level for x in level])
+            individuals = np.concatenate([x[0] for x in level])
+            predictions = np.concatenate([x[1] for x in level])
             
             #Create dataframe
             predictions_top1 = np.argmax(predictions, 1)    
--- tests/test_multi_stage.py
+++ tests/test_multi_stage.py
@@ -27,7 +27,7 @@ def test_predict(config, dm):
 
 def test_gather_predictions(config, dm, comet_logger):
     m  = multi_stage.MultiStage(train_df=dm.train, test_df=dm.test, crowns=dm.crowns, config=config)
-    trainer = Trainer(fast_dev_run=True)
+    trainer = Trainer(fast_dev_run=False)
     output = trainer.predict(m, dataloaders=m.val_dataloader())
     predictions = m.gather_predictions(predict_df=output, crowns=dm.crowns)    
     predictions.shape[0] == config["batch_size"]

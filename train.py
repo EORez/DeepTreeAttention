c5212afc540777729cb18ca17fa13a4ef24c0813
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-05-04 10:05:40 -0700

    dataframe didn't match

--- train.py
+++ train.py
@@ -162,7 +162,7 @@ for min_train in [5, 10, 20, 30, 50, 100, 150, 200, 500]:
     micro_list.append(micro_acc)
     macro_list.append(macro_acc)
 
-pd.DataFrame({"min_train_samples":[10, 20, 30, 50, 100, 200, 500, 1000],
+pd.DataFrame({"min_train_samples":[5, 10, 20, 30, 50, 100, 150, 200, 500],
               "micro_acc":micro_list,
               "macro_acc":macro_list}
              ).to_csv("results/all_sites_train_curve.csv")

6b0f7a582fde4266ec62f7c6856cea56264ac39f
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-05-06 11:12:49 -0700

    update data and comet logging

--- SLURM/experiment.sh
+++ SLURM/experiment.sh
@@ -23,6 +23,8 @@ source activate DeepTreeAttention
 cd ~/DeepTreeAttention/
 
 #get branch and commit name
+export COMET_LOGGING_CONSOLE=info
+
 branch_name=$((git symbolic-ref HEAD 2>/dev/null || echo "(unnamed branch)")|cut -d/ -f3-)
 commit=$(git log --pretty=format:'%H' -n 1)
 python train.py $branch_name $commit

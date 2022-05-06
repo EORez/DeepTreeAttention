273145a5573a6e3e5800ec8bf72bc7bcbf9e75e6
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-05-06 11:51:00 -0700

    seperate learning rates

--- src/models/multi_stage.py
+++ src/models/multi_stage.py
@@ -203,7 +203,7 @@ class MultiStage(LightningModule):
         """Create a optimizer for each level"""
         optimizers = []
         for x, ds in enumerate(self.train_datasets):
-            optimizer = torch.optim.Adam(self.models[x].parameters(), lr=self.config["lr"], weight_decay=0.001)
+            optimizer = torch.optim.Adam(self.models[x].parameters(), lr=self.config["lr_{}".format(x)], weight_decay=0.001)
             scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
                                                              mode='min',
                                                              factor=0.75,

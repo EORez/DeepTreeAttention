b9a930a243b2afa5dc67ce86228394f4266fc7bb
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-06-04 08:02:40 -0700

    sample later

--- src/data.py
+++ src/data.py
@@ -413,19 +413,17 @@ class TreeData(LightningDataModule):
             if self.comet_logger:
                 self.comet_logger.experiment.log_parameter("Species after crop generation",len(annotations.taxonID.unique()))
                 self.comet_logger.experiment.log_parameter("Samples after crop generation",annotations.shape[0])
-                
-            if self.config["new_train_test_split"]:
+    `        
+            if self.config["existing_test_csv"]:
+                existing_test = pd.read_csv(self.config["existing_test_csv"])
+                self.test = annotations[annotations.individualID.isin(existing_test.individualID)]
+                self.train = annotations[~annotations.individualID.isin(self.test)]
+                self.train = self.train[self.train.taxonID.isin(self.test.taxonID)]
+            else:
                 self.train, self.test = train_test_split(annotations, config=self.config, client=self.client) 
                 
                 self.train.to_csv("{}/train.csv".format(self.data_dir))
                 self.test.to_csv("{}/test.csv".format(self.data_dir))
-                
-            else:
-                previous_train = pd.read_csv("{}/train.csv".format(self.data_dir))
-                previous_test = pd.read_csv("{}/test.csv".format(self.data_dir))
-                
-                self.train = annotations[annotations.individualID.isin(previous_train.individualID)]
-                self.test = annotations[annotations.individualID.isin(previous_test.individualID)]
                 
             # Capture discarded species
             individualIDs = np.concatenate([self.train.individualID.unique(), self.test.individualID.unique()])
